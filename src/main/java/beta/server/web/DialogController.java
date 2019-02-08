2929ed714566b69d7261cc0a66395ac1a913f2c2
alexander.lohmann <alexander.lohmann@WS-017.cybertron.global>
2019-02-08 13:43:55 +0100

    Erstellen eines Kontakts mit Dialog mit Optionalen Adressen und Kommunikationen. Überprüfung des RegEX.

--- src/main/java/beta/server/web/DialogController.java
+++ src/main/java/beta/server/web/DialogController.java
@@ -5,6 +5,8 @@
  */
 package beta.server.web;
 
+import beta.server.entity.Address;
+import beta.server.entity.Communication;
 import beta.server.entity.Contact;
 import beta.server.entity.Sex;
 import java.io.Serializable;
@@ -21,9 +23,32 @@ import javax.inject.Named;
 public class DialogController implements Serializable {
 
     /**
+     * String to use for Pattern.
+     */
+    private String mask;
+
+    /**
+     * String to use for Communication type input.
+     */
+    private String type;
+
+    /**
+     * String to use ofr Communication identifier input.
+     */
+    private String identifier;
+
+    /**
      * String to use for Title input.
      */
     private String title;
+    /**
+     * Contact to save in ContactList after inputs.
+     */
+    private Contact contact = new Contact();
+    /**
+     * Adress to save into adresslist after inputs.
+     */
+    private Address address = new Address();
 
     /**
      * String to use for First Name input.
@@ -37,6 +62,32 @@ public class DialogController implements Serializable {
      * String to use for sex input setup.
      */
     private String sex = "";
+    /**
+     * String with the violationMessage to ouput on Website if Contact is not
+     * valid.
+     */
+    private String violationMessage;
+    /**
+     * String to use for street input.
+     */
+    private String street;
+
+    /**
+     * String to use for city input.
+     */
+    private String city;
+    /**
+     * String to use for zipCode input.
+     */
+    private String zipCode;
+    /**
+     * List to save inputed Communication in to use later for an new Contact.
+     */
+    private ArrayList<Communication> communications = new ArrayList<>();
+    /**
+     * List to save inputed Adress in to use later for an new Contact.
+     */
+    private ArrayList<Address> addresses = new ArrayList<>();
 
     /**
      * ArrayList to store new Contacts in.
@@ -44,6 +95,18 @@ public class DialogController implements Serializable {
     private ArrayList<Contact> contacts = new ArrayList<>();
 
     // Getter
+    public String getMask() {
+        return mask;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
     public String getTitle() {
         return title;
     }
@@ -64,7 +127,35 @@ public class DialogController implements Serializable {
         return contacts;
     }
 
+    public String getViolationMessage() {
+        return violationMessage;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
     //Setter
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -81,17 +172,109 @@ public class DialogController implements Serializable {
         this.sex = sex;
     }
 
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setViolationMessage(String violationMessage) {
+        this.violationMessage = violationMessage;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    /**
+     * Sets the input to an communication.
+     */
+    public void createNewCommunication() {
+        Communication communication = new Communication();
+        if ("Phone".equals(type)) {
+            communication.setType(Communication.Type.PHONE);
+        } else if ("Mobile".equals(type)) {
+            communication.setType(Communication.Type.MOBILE);
+        } else if ("Fax".equals(type)) {
+            communication.setType(Communication.Type.FAX);
+        } else if ("ICQ".equals(type)) {
+            communication.setType(Communication.Type.ICQ);
+        } else if ("Email".equals(type)) {
+            communication.setType(Communication.Type.EMAIL);
+        } else if ("Facebook".equals(type)) {
+            communication.setType(Communication.Type.FACEBOOK);
+        } else {
+            communication.setType(null);
+        }
+        communication.setIdentifier(identifier);
+        communications.add(communication);
+        System.out.println(communication.getIdentifier() + communication.getType());
+    }
+    /**
+     * Sets the Mask to use for input Pattern.
+     */
+    public void setsMask() {  
+        if ("Phone".equals(type)) {
+            mask = Communication.PHONE_PATTERN;
+        } else if ("Mobile".equals(type)) {
+            mask = Communication.PHONE_PATTERN;
+        } else if ("Fax".equals(type)) {
+            mask = Communication.PHONE_PATTERN;
+        } else if ("Email".equals(type)) {
+            mask = Communication.EMAIL_PATTERN;
+        } else {
+            mask = "";
+        }
+        System.out.println(mask);
+    }
+
+    /**
+     * Sets the inputs to an address.
+     */
+    public void createNewAddress() {
+        address.setStreet(street);
+        address.setCity(city);
+        address.setZipCode(zipCode);
+        addresses.add(address);
+    }
+
+    /**
+     * Saves the inputed Strings to a new Contact when there is no
+     * violationMessage and holds it in contacts ArrayList.
+     */
     public void createNewContact() {
-        
         Sex sexs;
         if ("Female".equals(sex)) {
             sexs = Sex.FEMALE;
-        } else {
+        } else if ("Male".equals(sex)) {
             sexs = Sex.MALE;
+        } else {
+            sexs = null;
         }
-        Contact blub = new Contact(sexs,title,firstName,lastName);
-        if (blub.getViolationMessage() == null){
-        contacts.add(blub);
+        contact.setSex(sexs);
+        contact.setFirstName(firstName);
+        contact.setLastName(lastName);
+        for (Address addresse : addresses) {
+            contact.getAddresses().add(addresse);
         }
+        for (Communication communication : communications) {
+            contact.getCommunications().add(communication);
+        }
+        if (contact.getViolationMessage() == null) {
+            contacts.add(contact);
+        }
+        violationMessage = contact.getViolationMessage();
+        communications = new ArrayList();
+        addresses = new ArrayList();
     }
 }

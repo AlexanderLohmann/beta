1b789175a8bf8cbce0d08e5f0ab7c8a6786b713f
alexander.lohmann <alexander.lohmann@WS-017.cybertron.global>
2019-02-27 12:33:48 +0100

    Änderungswünsche Oli, Unfertige Version / Zweck Wechsel des Arbeitsgerätes

--- src/main/java/beta/server/web/AufgabeZweiDialogController.java
+++ src/main/java/beta/server/web/AufgabeZweiDialogController.java
@@ -15,26 +15,19 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
- * Controller for the Aufgabe 2 website checks if the Contact is valid then saves it
- * in a List. For incorrect input it has Violationmessages.
+ * Controller for the Aufgabe 2 website checks if the Contact is valid then
+ * saves it in a List. For incorrect input it has Violationmessages.
  *
  * @author alexander.lohmann
  */
-@Named
+@Named("DialogController")
 @ViewScoped
-public class DialogController implements Serializable {
+public class AufgabeZweiDialogController implements Serializable {
 
-    /**
-     * String to use for violation Message.
-     */
-    private String violateMessage = "Bitte zuerst eine Kommunikationsart auswählen";
-    /**
-     * String to use for Pattern.
-     */
-    private String mask;
     /**
      * String with the violationMessage to ouput on Website if Contact is not
      * valid.
@@ -48,10 +41,7 @@ public class DialogController implements Serializable {
      * Adress to save into adresslist after inputs.
      */
     private Address address = new Address();
-    /**
-     * Communication to save into communicationlist after inputs.
-     */
-    private Communication communication = new Communication();
+
     /**
      * List to save inputed Communication in to use later for an new Contact.
      */
@@ -74,16 +64,16 @@ public class DialogController implements Serializable {
     private final List<String> sexs = new ArrayList<>();
 
     // Getter
-    public String getViolateMessage() {
-        return violateMessage;
-    }
-
-    public String getMask() {
-        return mask;
-    }
-
     public String getViolationMessage() {
         return violationMessage;
+    }
+
+    public ArrayList<Communication> getCommunications() {
+        return communications;
+    }
+
+    public ArrayList<Address> getAddresses() {
+        return addresses;
     }
 
     public Contact getContact() {
@@ -92,10 +82,6 @@ public class DialogController implements Serializable {
 
     public Address getAddress() {
         return address;
-    }
-
-    public Communication getCommunication() {
-        return communication;
     }
 
     public ArrayList<Contact> getContacts() {
@@ -124,48 +110,6 @@ public class DialogController implements Serializable {
     }
 
     /**
-     * Sets the chosen Communication type to the current communication. When the
-     * input is valid it saves the Communication in an List.
-     */
-    public void createNewCommunication() {
-        communications.add(communication);
-        communication = new Communication();
-    }
-
-    /**
-     * Sets the Mask to use for input Pattern and the violation Message for that
-     * pattern.
-     */
-    public void setsMaskAndVioMessage() {
-        if (communication.getType() != null) {
-            if ("PHONE".equals(communication.getType().name())) {
-                mask = Communication.PHONE_PATTERN;
-                violateMessage = "Bitte mit folgendem Muster Eingeben : +Ländervorwahl Vorwahl Nummer ,Beispiel: +49 40 898989";
-            } else if ("MOBILE".equals(communication.getType().name())) {
-                mask = Communication.PHONE_PATTERN;
-                violateMessage = "Bitte mit folgendem Muster Eingeben : +Ländervorwahl Vorwahl Nummer ,Beispiel: +49 40 898989";
-            } else if ("FAX".equals(communication.getType().name())) {
-                mask = Communication.PHONE_PATTERN;
-                violateMessage = "Bitte mit folgendem Muster Eingeben : +Ländervorwahl Vorwahl Nummer ,Beispiel: +49 40 898989";
-            } else if ("EMAIL".equals(communication.getType().name())) {
-                mask = Communication.EMAIL_PATTERN;
-                violateMessage = "Bitte mit folgendem Muster Eingeben : beispiel@beispiel.beispiel";
-            } else if ("ICQ".equals(communication.getType().name())) {
-                mask = "^(?=.*\\d).+$";
-                violateMessage = "Bitte eine Gültige Nummer eingeben.";
-            } else if ("FACEBOOK".equals(communication.getType().name())) {
-                mask = "[A-Za-z0-9]{1,999}";
-                violateMessage = "Bitte gültigen Benutzernamen oder Email eingeben.";
-            } else if ("SKYPE".equals(communication.getType().name())) {
-                mask = "[A-Za-z0-9]{1,999}";
-                violateMessage = "Bitte gültigen Benutzernamen oder Email eingeben.";
-            } else {
-                violateMessage = "Bitte zuerst eine Kommunikationsart auswählen";
-            }
-        }
-    }
-
-    /**
      * Sets the inputs to an address.
      */
     public void createNewAddress() {
@@ -178,13 +122,6 @@ public class DialogController implements Serializable {
      * violationMessage and holds it in contacts ArrayList.
      */
     public void createNewContact() {
-        if ("FEMALE".equals(contact.getSex().FEMALE.name())) {
-            contact.setSex(Sex.FEMALE);
-        } else if ("MALE".equals(contact.getSex().MALE.name())) {
-            contact.setSex(Sex.MALE);
-        } else {
-            contact.setSex(null);
-        }
         for (Address addresse : addresses) {
             contact.getAddresses().add(addresse);
         }
